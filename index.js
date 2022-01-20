59e2e8417723d2e9d6be4fc80830fec8ba126b9f
Dawid Kawka <d.kawka@op.pl>
2022-01-20 17:19:58 +0100

    add data information for server

--- index.js
+++ index.js
@@ -19,6 +19,41 @@ function startApp() {
 }
 
 function getWeatherData() {
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`;
-    console.log(url)
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${apiKey}`;
+    console.log(url);
+
+    fetch(url)
+        .then(function (response) {
+            response.json().then(function (data) {
+                console.log(data);
+                updateWeatherData(data);
+            });
+        });
+}
+
+function updateWeatherData(data) {
+    const city = data.name;
+    document.getElementById("city").innerHTML = city;
+
+    const temp = data.main.temp.toFixed();
+    document.getElementById("temp").innerHTML = temp + " <sup>o</sup>C";
+
+    const humi = data.main.humidity;
+    document.getElementById("humidity").innerHTML = humi + "%";
+
+    const press = data.main.pressure;
+    document.getElementById("pressure").innerHTML = press + "hPa";
+
+    const clouds = data.clouds.all;
+    document.getElementById("cloudsPerc").innerHTML = clouds + "%";
+
+    const wind = data.wind.speed.toFixed();
+    document.getElementById("wind").innerHTML = wind + " km/h";
+
+    const sunrise = new Date(data.sys.sunrise * 1000);
+    document.getElementById("sunRise").innerHTML = sunrise.getHours() + ":" + sunrise.getMinutes();
+
+    const sunset = new Date(data.sys.sunset * 1000);
+    document.getElementById("sunSet").innerHTML = sunset.getHours() + ":" + sunset.getMinutes();
+
 }
